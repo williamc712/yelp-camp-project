9fa70e889e52a9cf21037c16f7c05b19bb304966
William Chandra <williamchandra142@gmail.com>
2022-09-11 16:56:52 +0800

    add start

--- app.js
+++ app.js
@@ -154,6 +154,7 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(8080, ()=>{
-    console.log('LISTENING ON PORT 8080')
+const port = process.env.PORT || '8080';
+app.listen(port, ()=>{
+    console.log(`LISTENING ON PORT ${port}`);
 })
